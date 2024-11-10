464fb5a26f50ff61d5da613539231824ab94af65
Parampreet Singh <rubercuber.30@gmail.com>
2024-11-11 03:16:54 +0530

    Removed unwanted files in frontend

--- frontend/src/app/update-emails/page.tsx
+++
@@ -1,9 +0,0 @@
-import UpdateEmailBox from '../update_emails/components/UpdateEmailBox';
-
-export default function UpdateEmailsPage() {
-    return (
-        <div className="min-h-screen bg-gray-50 py-12">
-            <UpdateEmailBox />
-        </div>
-    );
-} 
